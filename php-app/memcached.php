81376835e6db97ee1b8619d780926c26f090dd20
Mindaugas Milius <milius@eventit.ag>
2021-09-16 08:31:33 +0200

    part 6 | minor changes

--- php-app/memcached.php
+++ php-app/memcached.php
@@ -4,7 +4,8 @@ $memcache = new Memcached();
 $memcache->addServer("memcached_test", 11211);
 
 if (isset($_GET['set'])) {
-    $memcache->set("test", $_GET['set']);
+    $set = $memcache->set("test", $_GET['set']);
+    echo sprintf('set success? %s <br>', var_export($set, true));
 }
 
 echo 'current test value: ';
